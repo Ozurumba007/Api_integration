55ea2c4122f93be913a04bbd4695290b90fb0c2b
Ozurumba Onwuasoanya <onwuasoanyaozurumba007@gmail.com>
2023-09-12 15:11:47 +0100

    added a listview to the home_page file \ created a method called getData to fetch data from the internet

--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../models/post.dart';
+
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
@@ -8,11 +10,34 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  List<Post>? posts;
+  var isLoaded = false;
+
+  @override
+  void initState() {
+    super.initState();
+
+    // fetch data from API
+    getData();
+  }
+
+  getData() async {
+    // posts = await
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Post'),
+        title: const Text('Post'),
+      ),
+      body: ListView.builder(
+        itemCount: 10,
+        itemBuilder: (context, index) {
+          return Container(
+            child: Text('Hi'),
+          );
+        },
       ),
     );
   }
